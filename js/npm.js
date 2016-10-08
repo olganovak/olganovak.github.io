37d02ed2d7e57e4c5210adf9a9a2fbe5466fc482
olganovak <olnewlife@gmail.com>
2016-10-08 03:20:20 +0300

    final

--- js/npm.js
+++ js/npm.js
@@ -11,3 +11,36 @@ require('../../js/popover.js')
 require('../../js/scrollspy.js')
 require('../../js/tab.js')
 require('../../js/affix.js')
+$(document).ready(function() {  
+ $("#validate").keyup(function(){
+    
+    var email = $("#validate").val();
+  
+    if(email != 0)
+    {
+    if(isValidEmailAddress(email))
+    {
+    $("#validEmail").css({
+  "background-image": "url('validYes.png')"
+    });
+    } else {
+    $("#validEmail").css({
+  "background-image": "url('validNo.png')"
+    });
+    }
+    } else {
+    $("#validEmail").css({
+  "background-image": "none"
+    }); 
+    }
+  
+    });
+  
+    });
+  
+    function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+    }
+  
+  </script>  
